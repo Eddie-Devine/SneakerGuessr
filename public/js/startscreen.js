1fe7ad9dfc0b3bf761dee5af7bcb0cc7fe639d9e
Eddie Devine <edevine000@gmail.com>
2022-05-20 19:00:32 -0700

    Added more play buttons.

--- public/js/startscreen.js
+++ public/js/startscreen.js
@@ -1,19 +1,19 @@
-const divs = document.querySelectorAll('.checkboxes');
-let currentDiv = 0;
-let maxChildren;
+const divs = document.querySelectorAll('.checkboxes'); //each checkbox column
+let currentDiv = 0; //current column being added to
+let maxChildren; //max number of checkboxes a column can hold
+const boost = document.querySelector('.gamemodes').offsetHeight + 50; //how much more early the column will fill to leave room for the buttons (pixels)
 sneakers.forEach(sneaker => {
-    divs[currentDiv].innerHTML += `<label class="toggle"><input class="toggle__input" type="checkbox" name="${sneaker}"><span class="toggle__label"><span class="toggle__text">${sneaker}</span></span></label>`;
-    const distance = divs[currentDiv].getBoundingClientRect().bottom;
-    console.log(distance);
+    divs[currentDiv].innerHTML += `<label class="toggle"><input class="toggle__input" type="checkbox" name="${sneaker}" checked><span class="toggle__label"><span class="toggle__text">${sneaker}</span></span></label>`;
+    const distance = divs[currentDiv].getBoundingClientRect().bottom; //distance to bottom of screen
 
+    //if on the first column measure by distance from bottom
     if(currentDiv == 0){
-        if(distance >= window.innerHeight - ((15 / 100) * window.innerHeight)){
-            console.log('too far!');
+        if(distance >= window.innerHeight - boost){
             maxChildren = divs[currentDiv].childElementCount;
             currentDiv++;
         }
     }
-    else{
+    else{ //otherwise measure by number of children
         if(divs[currentDiv].childElementCount == maxChildren) currentDiv++;
     }
 });
@@ -24,4 +24,16 @@ document.querySelector('.start').addEventListener('click', () => {
         if(checkbox.checked) selectedShoes.push(checkbox.name);
     });
     console.log(selectedShoes);
+    
+    document.cookie = selectedShoes.join('_');
+
+
+    // function setCookie(cname, cvalue, exdays) {
+    //     const d = new Date();
+    //     d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    //     let expires = "expires="+ d.toUTCString();
+    //     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    // }
+
+    // setCookie('sneakers', 'hey');
 });
