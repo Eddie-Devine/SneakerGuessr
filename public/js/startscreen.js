c92b71b84dccc6e0ed93551b81626ca7be6959a4
Eddie Devine <edevine000@gmail.com>
2022-05-21 19:55:07 -0700

    Started name guessing gamemode and allowed user to save sneaker preferences.

--- public/js/startscreen.js
+++ public/js/startscreen.js
@@ -2,8 +2,9 @@ const divs = document.querySelectorAll('.checkboxes'); //each checkbox column
 let currentDiv = 0; //current column being added to
 let maxChildren; //max number of checkboxes a column can hold
 const boost = document.querySelector('.gamemodes').offsetHeight + 50; //how much more early the column will fill to leave room for the buttons (pixels)
+
 sneakers.forEach(sneaker => {
-    divs[currentDiv].innerHTML += `<label class="toggle"><input class="toggle__input" type="checkbox" name="${sneaker}" checked><span class="toggle__label"><span class="toggle__text">${sneaker}</span></span></label>`;
+    divs[currentDiv].innerHTML += `<label class="toggle"><input class="toggle__input" type="checkbox" name="${sneaker}" ${!document.cookie || document.cookie.includes('_' + sneaker + '_') ? 'checked' : ''}><span class="toggle__label"><span class="toggle__text">${sneaker}</span></span></label>`;
     const distance = divs[currentDiv].getBoundingClientRect().bottom; //distance to bottom of screen
 
     //if on the first column measure by distance from bottom
@@ -23,17 +24,6 @@ document.querySelector('.start').addEventListener('click', () => {
     document.querySelectorAll('input[type=checkbox]').forEach(checkbox => {
         if(checkbox.checked) selectedShoes.push(checkbox.name);
     });
-    console.log(selectedShoes);
-    
-    document.cookie = selectedShoes.join('_');
-
-
-    // function setCookie(cname, cvalue, exdays) {
-    //     const d = new Date();
-    //     d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    //     let expires = "expires="+ d.toUTCString();
-    //     document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    // }
-
-    // setCookie('sneakers', 'hey');
+    //underscores added for consistancy when reading saved shoes
+    document.cookie = '_' + selectedShoes.join('_') + '_';
 });
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ const SneaksAPI = require('sneaks-api');
 const sneaks = new SneaksAPI();
 const express = require('express');
 const path = require('path');
+const req = require('express/lib/request');
 
 //express server setup
 const app = express();
 const port = process.env.PORT || 5000;
+//routes for files
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', res => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('/name', (req, res) => res.sendFile(path.join(__dirname, 'public', 'name.html')));
+//start listening for requests
 app.listen(port, () => console.log(`Listening on port ${port}.`));
 
 // sneaks.getProducts("vans", 10, function(err, products){
