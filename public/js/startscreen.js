45759a6d3e07d51471809c5ae8ca988d6032eea4
Eddie Devine <edevine000@gmail.com>
2022-06-18 19:05:43 -0700

    Fixed start screen height bug.

--- public/js/startscreen.js
+++ public/js/startscreen.js
@@ -3,20 +3,23 @@ let currentDiv = 0; //current column being added to
 let maxChildren; //max number of checkboxes a column can hold
 const boost = document.querySelector('.gamemodes').offsetHeight + 50; //how much more early the column will fill to leave room for the buttons (pixels)
 
-sneakers.forEach(sneaker => {
-    divs[currentDiv].innerHTML += `<label class="toggle"><input class="toggle__input" type="checkbox" name="${sneaker}" ${!document.cookie || document.cookie.includes('_' + sneaker + '_') ? 'checked' : ''}><span class="toggle__label"><span class="toggle__text">${sneaker}</span></span></label>`;
-    const distance = divs[currentDiv].getBoundingClientRect().bottom; //distance to bottom of screen
-
-    //if on the first column measure by distance from bottom
-    if(currentDiv == 0){
-        if(distance >= window.innerHeight - boost){
-            maxChildren = divs[currentDiv].childElementCount;
-            currentDiv++;
+document.querySelector('.logo').addEventListener('load', () => { //takes out image loading time variable
+    sneakers.forEach(sneaker => {
+        divs[currentDiv].innerHTML += `<label class="toggle"><input class="toggle__input" type="checkbox" name="${sneaker}" ${!document.cookie || document.cookie.includes('_' + sneaker + '_') ? 'checked' : ''}><span class="toggle__label"><span class="toggle__text">${sneaker}</span></span></label>`;
+        const distance = divs[currentDiv].getBoundingClientRect().bottom; //distance to bottom of screen
+    
+        //if on the first column measure by distance from bottom
+        if(currentDiv == 0){
+            if(distance >= window.innerHeight - boost){
+                maxChildren = divs[currentDiv].childElementCount;
+                console.log(maxChildren);
+                currentDiv++;
+            }
         }
-    }
-    else{ //otherwise measure by number of children
-        if(divs[currentDiv].childElementCount == maxChildren) currentDiv++;
-    }
+        else{ //otherwise measure by number of children
+            if(divs[currentDiv].childElementCount == maxChildren) currentDiv++;
+        }
+    });
 });
 
 const startButtons = document.querySelectorAll('.start');
